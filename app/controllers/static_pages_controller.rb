e99745be60fe51e621349b0a2e9e1716567700d6
DiemQuynh21 <lediemquynh999@gmail.com>
2021-03-14 16:36:46 +0700

    Initial project

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+class StaticPagesController < ApplicationController
+  def home
+    @products = Product.order_alphabet_name.paginate(page: params[:page],
+                                                     per_page: Settings.page.per_page)
+  end
+end
